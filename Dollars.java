d937e6fb55dffb6be5fdd85128285308565fbbef
John Claessens <JohnClaessens@users.noreply.github.com>
2017-06-19 23:45:33 -0500

    Add files via upload

---
+++ Dollars.java
@@ -0,0 +1,39 @@
+import javax.swing.*;
+
+public class Dollars {
+
+  public static void main(String [] args){
+      
+    //userInput and intInput variable declarations 
+    String userInput;
+    int intInput = 0;
+    int intHundred = 0;
+    int intFifty = 0;
+    int intTwenty = 0;
+    int intTen = 0;
+    int intFive = 0;
+    int intOne = 0;
+    
+    //prompt user for input and respond
+    userInput = JOptionPane.showInputDialog(null, "Please enter a dollar amount:"); 
+    intInput = Integer.parseInt(userInput);
+    while (intInput!= 0) {
+        intHundred = intInput/100;
+        intInput = intInput % 100;
+        intFifty = intInput/50;
+        intInput = intInput % 50;
+        intTwenty = intInput/20;
+        intInput = intInput % 20;
+        intTen = intInput/10;
+        intInput = intInput % 10;
+        intFive = intInput/5;
+        intInput = intInput % 5;
+        intOne = intInput /1;
+        intInput = intInput % 1;
+    }
+    System.out.println("\nYou entered the dollar amount: $" + userInput);
+    System.out.println("Currency breakdown is:\n\n\t" + intHundred + "\tHundreds\n\t" + intFifty + "\tFifties\n\t" + intTwenty + "\tTwenties\n\t" + intTen + "\tTens\n\t" + intFive + "\tFives\n\t" + intOne + "\tOnes\n\t");
+
+    System.exit(0);
+  }
+}
