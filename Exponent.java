bc0dccc42e3c0c92f412ca0d5ce2970f09d7b152
John Claessens <JohnClaessens@users.noreply.github.com>
2017-06-24 17:03:34 -0500

    Add files via upload

--- Exponent.java
+++ Exponent.java
@@ -11,32 +11,44 @@ public class Exponent{
         public static void main(String [] args){
             //declaring variables
             String userInput;
-            double numToSquare;
-            double numSquared;            
+            double numToCalculate;
+            double numCalculated;            
             
             //get user to enter a double value
             userInput = JOptionPane.showInputDialog(null, "Please enter a number to square:"); 
             //convert the string input to a double
-            numToSquare = Double.parseDouble(userInput);
+            numToCalculate = Double.parseDouble(userInput);
             
             //to access the non-static method create an instance of Exponent
             Exponent e = new Exponent();    
             
-            //assign a value to the numSquared by passing a value to the squareValue method
-            numSquared = e.squareValue(numToSquare);
+            //assign a value to the numCalculated by passing a value to the squareValue method
+            numCalculated = e.squareValue(numToCalculate);
             
             //display the output in a dialog box
-            JOptionPane.showMessageDialog(null,"The number " + numToSquare + " squared is " + numSquared);
+            JOptionPane.showMessageDialog(null,"The number " + numToCalculate + " squared is " + numCalculated);
+            
+            //assign a value to the numCalculated by passing a value to the squareValue method
+            numCalculated = e.cubeValue(numToCalculate);
+            
+            //display the output in a dialog box
+            JOptionPane.showMessageDialog(null,"The number " + numToCalculate + " cubed is " + numCalculated);
             
             //exit the program
             System.exit(0);
         }//End of Main 
         
         public double squareValue(double param1){
-            //take the value passed into the method and calculate the square of thsi number
+            //take the value passed into the method and calculate the square value and return
             //param1 = param1 * param1;
             //this can also be done using the Math.pow method
             param1 = Math.pow(param1,2);
+            return param1;     
+        }//End of squareValue
+        
+        public double cubeValue(double param1){
+            //take the value passed into the method and calculate cube value and return
+            param1 = Math.pow(param1,3);
             return param1;     
         }//End of squareValue
         
