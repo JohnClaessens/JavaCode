883dffdf77ea816a8e1af4b3568046dc074dee9a
John Claessens <JohnClaessens@users.noreply.github.com>
2017-06-26 17:10:25 -0500

    Add files via upload

---
+++ CollegeStudent.java
@@ -0,0 +1,59 @@
+//  John Claessens
+//  Assignment 2.2
+//  CollegeStudent Class file
+//  Bellevue University
+//  25 June 2017
+
+import java.util.*;
+
+public class CollegeStudent{
+    
+    //declaring variables
+    public String firstName;
+    public String lastName;
+    public GregorianCalendar enrollmentDate;
+    public GregorianCalendar projectedGradDate;
+    
+    /*= new GregorianCalendar(
+            enrollmentDate.get(enrollmentDate.YEAR),
+            enrollmentDate.get(enrollmentDate.MONTH),
+            enrollmentDate.get(enrollmentDate.DAY_OF_MONTH));
+    */
+    //constructor
+    public CollegeStudent(String firstName, String lastName, GregorianCalendar enrollmentDate){
+        firstName = firstName;
+        lastName = lastName;
+        enrollmentDate = enrollmentDate;
+        projectedGradDate = new GregorianCalendar(
+                                enrollmentDate.get(enrollmentDate.YEAR),
+                                enrollmentDate.get(enrollmentDate.MONTH),
+                                enrollmentDate.get(enrollmentDate.DAY_OF_MONTH));
+        projectedGradDate.add(enrollmentDate.YEAR,4);    
+    }
+    
+    //getters and setters
+    public String getFirstName(){
+        return firstName;
+    }
+    
+    public void setFirstName(String firstName){
+        firstName = firstName;
+    }
+    
+    public String getLastName(){
+        return lastName;
+    }
+    
+    public void setLastName(String firstName){
+        lastName = lastName;
+    }
+    
+    public GregorianCalendar getEnrollmentDate(){
+        return enrollmentDate;
+    }
+    
+    public void setEnrollmentDate(GregorianCalendar enrollmentDate){
+        enrollmentDate = enrollmentDate;
+    }
+    
+}//End of CollegeStudent class
